f8c82eb8d6ccbd1f85671daed4e75db442e84458
Rick N <richard.norman.id@gmail.com>
2021-11-21 16:45:22 -0700

    Initial UI formatting

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ function hash1() {
     //var shasum = crypto.createHash('sha1')
     //shasum.update('foo')
     //alert(shasum.digest('bar'))
-    alert('hi world')
+    alert('Cracking weak passwords in progress...')
+
+}
+
+function hash2() {
+    //var shasum = crypto.createHash('sha1')
+    //shasum.update('foo')
+    //alert(shasum.digest('bar'))
+    alert('Cracking strong passwords in progress...')
 
 }
